a696789969f5bd0012a00870e8fe765da798a052
mellava <6126925+mellava@users.noreply.github.com>
2023-06-10 22:08:38 -0700

    Modify db connection

--- server/db/copy.sql
+++ server/db/copy.sql
@@ -1,4 +1,4 @@
-COPY products(id, name, slogan, description, category, default_price) FROM './csv/product.csv' DELIMITER ',' CSV HEADER;
+\copy products(id, name, slogan, description, category, default_price) FROM './csv/product.csv' DELIMITER ',' CSV HEADER;
 COPY styles(id, product_id, name, sale_price, original_price, default_style) FROM './csv/styles.csv' DELIMITER ',' CSV HEADER;
 COPY photos(id, style_id, url, thumbnail_url) FROM './csv/photos.csv' DELIMITER ',' CSV HEADER;
 COPY skus(id, style_id, size, quantity) FROM './csv/skus.csv' DELIMITER ',' CSV HEADER;
